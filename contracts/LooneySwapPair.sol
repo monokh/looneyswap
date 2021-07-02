a43e0106c60fdc81aaadd1dd0bed3b62ca5a53a0
monokh <mnokhb@gmail.com>
2021-07-02 15:00:47 +0100

    Mint liquidity token porportionally

--- contracts/LooneySwapPair.sol
+++ contracts/LooneySwapPair.sol
@@ -1,35 +1,51 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/utils/math/Math.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "hardhat/console.sol";
 import "./LooneySwapERC20.sol";
 
 contract LooneySwapPair is LooneySwapERC20 {
-  using SafeMath for uint256;
+  uint public constant INITIAL_SUPPLY = 10**5;
+
+  using SafeMath for uint;
 
   address public token0;
   address public token1;
 
-  uint256 public reserve0;
-  uint256 public reserve1;
+  uint public reserve0;
+  uint public reserve1;
 
   constructor(address _token0, address _token1) LooneySwapERC20() {
     token0 = _token0;
     token1 = _token1;
   }
 
-  function getReserves() public view returns (uint256 _reserve0, uint256 _reserve1) {
+  function getReserves() public view returns (uint _reserve0, uint _reserve1) {
     _reserve0 = reserve0;
     _reserve1 = reserve1;
   }
 
-  function add(uint256 token0Amount, uint256 token1Amount) public {
-    assert(IERC20(token0).transferFrom(msg.sender, address(this), token0Amount));
-    assert(IERC20(token1).transferFrom(msg.sender, address(this), token1Amount));
+  function add(uint amount0, uint amount1) public {
+    assert(IERC20(token0).transferFrom(msg.sender, address(this), amount0));
+    assert(IERC20(token1).transferFrom(msg.sender, address(this), amount1));
 
-    reserve0 = reserve0.add(token0Amount);
-    reserve1 = reserve1.add(token1Amount);
+    uint reserve0After = reserve0.add(amount0);
+    uint reserve1After = reserve1.add(amount1);
+
+    if (reserve0 == 0 && reserve1 == 0) {
+      _mint(msg.sender, INITIAL_SUPPLY);
+    } else {
+      uint currentSupply = totalSupply();
+      uint newSupplyGivenReserve0Ratio = reserve0After.mul(currentSupply).div(reserve0);
+      uint newSupplyGivenReserve1Ratio = reserve1After.mul(currentSupply).div(reserve1);
+      uint newSupply = Math.min(newSupplyGivenReserve0Ratio, newSupplyGivenReserve1Ratio);
+      _mint(msg.sender, newSupply - currentSupply);
+    }
+
+    reserve0 = reserve0After;
+    reserve1 = reserve1After;
   }
 }
--- test/LooneySwapPair.spec.ts
+++ test/LooneySwapPair.spec.ts
@@ -2,40 +2,67 @@ import { expect } from 'chai'
 import { ethers } from 'hardhat'
 import { Token } from '../typechain/Token'
 import { LooneySwapPair } from '../typechain/LooneySwapPair'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 
 describe("LooneySwapPair", function() {
   let token0: Token
   let token1: Token
+  let pair: LooneySwapPair
+  let accounts: SignerWithAddress[]
 
   beforeEach(async () => {
+    accounts = await ethers.getSigners()
+
     const Token = await ethers.getContractFactory('Token')
-    token0 = await Token.deploy('Bitcoin', 'BTC', 1000000000) as Token
+    token0 = await Token.deploy('Bitcoin', 'BTC', 100000000000) as Token
     token1 = await Token.deploy('USDC', 'USDC', 1000000000000000) as Token
     await token0.deployed()
     await token1.deployed()
+
+    const [, account1, account2, account3] = accounts
+
+    for (const account of [account1, account2, account3]) {
+      await (await token0.transfer(account.address, 200)).wait()
+      await (await token1.transfer(account.address, 10000000)).wait()
+    }
+
+    const LooneySwapPair = await ethers.getContractFactory("LooneySwapPair")
+    pair = await LooneySwapPair.deploy(token0.address, token1.address) as LooneySwapPair
+    await pair.deployed()
   })
 
   it("Should return initialized pair", async function() {
-    const LooneySwapPair = await ethers.getContractFactory("LooneySwapPair")
-    const pair: LooneySwapPair = await LooneySwapPair.deploy(token0.address, token1.address) as LooneySwapPair
-    await pair.deployed()
-
     expect(await pair.token0()).to.equal(token0.address)
     expect(await pair.token1()).to.equal(token1.address)
     expect(await pair.reserve0()).to.equal(0)
     expect(await pair.reserve1()).to.equal(0)
   })
 
-  it("Should add liquidity to reserves", async function() {
-    const LooneySwapPair = await ethers.getContractFactory("LooneySwapPair")
-    const pair: LooneySwapPair = await LooneySwapPair.deploy(token0.address, token1.address) as LooneySwapPair
-    await pair.deployed()
-
-    await (await token0.approve(pair.address, 1)).wait()
-    await (await token1.approve(pair.address, 50000)).wait()
-    await (await pair.add(1, 50000)).wait()
+  it("Should add initial liquidity to reserves", async function() {
+    await (await token0.connect(accounts[1]).approve(pair.address, 1)).wait()
+    await (await token1.connect(accounts[1]).approve(pair.address, 50000)).wait()
+    await (await pair.connect(accounts[1]).add(1, 50000)).wait()
 
     expect(await pair.reserve0()).to.equal(1)
     expect(await pair.reserve1()).to.equal(50000)
+    expect(await pair.totalSupply()).to.equal(100000) // Initial Supply
+    expect(await pair.balanceOf(accounts[1].address)).to.equal(100000)
+  })
+
+  it("Should mint correct amount", async function() {
+    await (await token0.connect(accounts[1]).approve(pair.address, 1)).wait()
+    await (await token1.connect(accounts[1]).approve(pair.address, 50000)).wait()
+    await (await pair.connect(accounts[1]).add(1, 50000)).wait()
+
+    await (await token0.connect(accounts[2]).approve(pair.address, 3)).wait()
+    await (await token1.connect(accounts[2]).approve(pair.address, 150000)).wait()
+    await (await pair.connect(accounts[2]).add(3, 150000)).wait()
+
+    expect(await pair.reserve0()).to.equal(4)
+    expect(await pair.reserve1()).to.equal(200000)
+    expect(await pair.totalSupply()).to.equal(400000)
+
+    expect(await pair.balanceOf(accounts[1].address)).to.equal(100000)
+    expect(await pair.balanceOf(accounts[2].address)).to.equal(300000)
   })
 })
